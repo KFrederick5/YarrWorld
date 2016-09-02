68a8bf2a537ea6715a6892d9a4add27963f9403c
kfrederick5 <tefsuncle@gmail.com>
2016-09-02 00:43:05 -0700

    Added images to file, created the UI, and started working on the button back end.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/yarrworld/MainActivity.java
@@ -1,13 +1,33 @@
 package edu.orangecoastcollege.cs273.kfrederick5.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button speakButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Remembers where the user last left off
         super.onCreate(savedInstanceState);
+
+        //Inflated (!) view on startup which is activity_main.xml
         setContentView(R.layout.activity_main);
+
+        //Referencing the button widget
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+        //Listener waiting for user interaction
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
