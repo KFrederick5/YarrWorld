b7d7d57e40e3cb032c6f4da22504f97b03c2729c
kfrederick5 <tefsuncle@hotmail.com>
2016-09-06 15:39:02 -0700

    Changed size of text

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/yarrworld/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class   MainActivity extends AppCompatActivity {
 
     private Button speakButton;
 
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         speakButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.toast_text, Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, R.string.toast_text, Toast.LENGTH_SHORT).show();
             }
         });
     }
